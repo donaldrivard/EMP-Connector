c9d1430c31a3a98293b03bcd79f57e181ab78e68
Donald Rivard <drivard@drivard-ltm1.internal.salesforce.com>
2020-12-24 11:35:56 -0500

    changed to test

---
+++ run.sh
@@ -0,0 +1,1 @@
+java -classpath target/emp-connector-0.0.1-SNAPSHOT-phat.jar com.salesforce.emp.connector.example.LoginExample edubrite.sub@salesforce.com.tidev1 Edubrite123! /event/UserDataChange__e
